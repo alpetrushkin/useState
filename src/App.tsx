c8cfc53781d8bb6ef34271318a7a585abc22e040
alpetrushkin <alpetrushkin1984@mail.ru>
2022-07-25 17:24:21 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   // let a = 1
+   let [a, setA] = useState(1)
+
+   const onClickHandler = () => {
+      setA(++a)
+      console.log(a)
+   }
+
+   const onClickReset = () => {
+      setA(0)
+      console.log(a)
+   }
+
+   return (
+      <div className="App">
+         <h1>{a}</h1>
+         <button onClick={onClickHandler}>number</button>
+         <button onClick={onClickReset}>0</button>
+      </div>
+   );
 }
 
 export default App;
